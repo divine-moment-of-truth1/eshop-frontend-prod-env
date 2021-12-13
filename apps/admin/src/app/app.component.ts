f35d1a76bb15ffeef331e7b75e50c1ed8c868538
andyshort101 <andy.short101@gmail.com>
2021-12-13 16:22:33 +0000

    Added scroll to top after clicking next page pagination button

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { UsersService } from '@bluebits/users';
 
 @Component({
-  selector: 'admin-root',
+  selector: 'ngshop-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'admin';
+export class AppComponent implements OnInit {
+    constructor(private usersService: UsersService, private router: Router) { }
+
+    title = 'ngshop';
+
+    ngOnInit() {
+        console.log("Created this.usersFacade.buildUserSession();")
+        this.usersService.initAppSession();
+
+        this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0)
+        });
+    }
+
 }
